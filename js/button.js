d6fc4fab9159ec530db19cf2d885de5a202fc85e
Luan de Castro Ribeiro <luan.ribeiro@estudante.ifgoiano.edu.br>
2022-11-28 09:20:16 -0300

    falksdflçkasdjklçsadf

---
+++ js/button.js
@@ -0,0 +1,9 @@
+let runner = document.getElementById("fugitivo")
+
+function runnerMissing(){
+    runner.style.display = "none";
+}
+
+function functionalButton(){
+    window.alert("Já sabia!")
+}
